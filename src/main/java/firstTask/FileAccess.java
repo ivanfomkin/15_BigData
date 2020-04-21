13b91f6fb056737734eeea6da16d5a7af00089f6
Ivan <ivan-fomkin@yandex.ru>
2020-04-22 00:03:37 +0300

    Add first task

--- src/main/java/firstTask/FileAccess.java
+++ src/main/java/firstTask/FileAccess.java
@@ -1,18 +1,34 @@
 package firstTask;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.*;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class FileAccess
-{
+public class FileAccess {
+    private Configuration configuration;
+    private FileSystem hdfs;
+    private String rootPath;
+
     /**
      * Initializes the class, using rootPath as "/" directory
      *
      * @param rootPath - the path to the root of HDFS,
-     * for example, hdfs://localhost:32771
+     *                 for example, hdfs://localhost:32771
      */
-    public FileAccess(String rootPath)
-    {
-
+    public FileAccess(String rootPath) throws IOException, URISyntaxException {
+        configuration = new Configuration();
+        configuration.set("dfs.client.use.datanode.hostname", "true");
+        System.setProperty("HADOOP_USER_NAME", "root");
+        this.rootPath = rootPath;
+        hdfs = FileSystem.get(
+                new URI(rootPath), configuration
+        );
     }
 
     /**
@@ -20,9 +36,14 @@ public class FileAccess
      *
      * @param path
      */
-    public void create(String path)
-    {
+    public void create(String path) throws IOException {
+        Path filePath = new Path(rootPath + "/" + path);
 
+        if (hdfs.exists(filePath)) {
+            System.out.println("Path " + filePath + " already exists!");
+        } else {
+            hdfs.createNewFile(filePath);
+        }
     }
 
     /**
@@ -31,9 +52,21 @@ public class FileAccess
      * @param path
      * @param content
      */
-    public void append(String path, String content)
-    {
+    public void append(String path, String content) throws IOException {
+        Path filePath = new Path(rootPath + "/" + path);
+        if (hdfs.exists(filePath) && hdfs.isFile(filePath)) {
+            hdfs.setReplication(filePath, (short) 1);
+            FSDataOutputStream file = hdfs.append(filePath);
+            BufferedWriter writer = new BufferedWriter
+                    (new OutputStreamWriter(file, "UTF-8"));
 
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            file.close();
+        } else {
+            System.out.println("This is not file or this file does not exist");
+        }
     }
 
     /**
@@ -42,10 +75,24 @@ public class FileAccess
      * @param path
      * @return
      */
-    public String read(String path)
-    {
-
-        return null;
+    public String read(String path) throws IOException {
+        var result = new StringBuilder();
+        Path filePath = new Path(rootPath + "/" + path);
+        if (hdfs.isFile(filePath) && hdfs.exists(filePath)) {
+            FSDataInputStream open = hdfs.open(filePath);
+            hdfs.setReplication(new Path(rootPath), (short) 1);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(open));
+//            reader.lines().forEach(l -> result.append(l));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            open.close();
+            reader.close();
+        } else {
+            result.append("This is not file or this file does not exist");
+        }
+        return result.toString();
     }
 
     /**
@@ -53,9 +100,13 @@ public class FileAccess
      *
      * @param path
      */
-    public void delete(String path)
-    {
-
+    public void delete(String path) throws IOException {
+        Path filePath = new Path(rootPath + "/" + path);
+        if (hdfs.exists(filePath)) {
+            hdfs.delete(filePath, true);
+        } else {
+            System.out.println(path + " does not exist");
+        }
     }
 
     /**
@@ -64,9 +115,8 @@ public class FileAccess
      * @param path
      * @return
      */
-    public boolean isDirectory(String path)
-    {
-        return false;
+    public boolean isDirectory(String path) throws IOException {
+        return hdfs.isDirectory(new Path(rootPath + "/" + path));
     }
 
     /**
@@ -75,8 +125,27 @@ public class FileAccess
      * @param path
      * @return
      */
-    public List<String> list(String path)
-    {
-        return null;
+    public List<String> list(String path) throws IOException {
+        String prefix = "";
+        if (!path.contains(rootPath)) prefix = rootPath + "/";
+        Path filesPath = new Path(prefix + path);
+        List<String> files = new ArrayList<>();
+        if (hdfs.exists(filesPath)) {
+            FileStatus[] fileStatuses = hdfs.listStatus(filesPath);
+            for (FileStatus fileStatus : fileStatuses) {
+                if (fileStatus.isDirectory()) {
+                    files.addAll(list(fileStatus.getPath().toString()));
+                } else {
+                    files.add(fileStatus.getPath().toString());
+                }
+            }
+        } else {
+            System.out.println("Incorrect path!");
+        }
+        return files;
+    }
+
+    public void close() throws IOException {
+        hdfs.close();
     }
 }
--- src/main/java/firstTask/Main.java
+++ src/main/java/firstTask/Main.java
@@ -1,53 +1,49 @@
 package firstTask;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-
-import java.io.BufferedWriter;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.URI;
-
-public class Main
-{
+public class Main {
     private static String symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String HADOOP_CONTAINER_ID = "56cccf022503"; //Указать ID Docker-контейнера тут
+    private static final String HADOOP_PATH = "hdfs://" + HADOOP_CONTAINER_ID + ":8020/";
 
-    public static void main(String[] args) throws Exception
-    {
-        Configuration configuration = new Configuration();
-        configuration.set("dfs.client.use.datanode.hostname", "true");
-        System.setProperty("HADOOP_USER_NAME", "root");
+    public static void main(String[] args) throws Exception {
 
-        FileSystem hdfs = FileSystem.get(
-            new URI("hdfs://HOST_NAME:8020"), configuration
-        );
-        Path file = new Path("hdfs://HOST_NAME:8020/test/file.txt");
 
-        if (hdfs.exists(file)) {
-            hdfs.delete(file, true);
-        }
+        FileAccess hadoop = new FileAccess(HADOOP_PATH);
 
-        OutputStream os = hdfs.create(file);
-        BufferedWriter br = new BufferedWriter(
-            new OutputStreamWriter(os, "UTF-8")
-        );
+        //Попробуем посоздавать файлы
+        hadoop.create("myFile");
+        hadoop.create("myfile/myfile");
+        hadoop.create("test.txt");
+        hadoop.create("myFile");
+        //Выведем их список
+        hadoop.list(HADOOP_PATH).forEach(System.out::println);
 
-        for(int i = 0; i < 10_000_000; i++) {
-            br.write(getRandomWord() + " ");
-        }
+        //Проверим, дериктория ли это
+        if (hadoop.isDirectory("myfile"))
+            System.out.println("myFile is directory");
+        else System.out.println("myFile is not directory");
 
-        br.flush();
-        br.close();
-        hdfs.close();
+        //Проверим методы read и append
+        System.out.println(hadoop.read("test.txt"));
+        hadoop.append("test.txt", "This is test text");
+        System.out.println(hadoop.read("test.txt"));
+        hadoop.append("test.txt", "This is second string");
+        System.out.println(hadoop.read("test.txt"));
+
+        //Удалим файлы
+        hadoop.delete("abc");
+        hadoop.delete("myFile");
+        hadoop.delete("myfile/myfile");
+        hadoop.delete("myfile");
+        hadoop.delete("test");
+        hadoop.delete("test.txt");
     }
 
-    private static String getRandomWord()
-    {
+    private static String getRandomWord() {
         StringBuilder builder = new StringBuilder();
         int length = 2 + (int) Math.round(10 * Math.random());
         int symbolsCount = symbols.length();
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             builder.append(symbols.charAt((int) (symbolsCount * Math.random())));
         }
         return builder.toString();
